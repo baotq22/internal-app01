636b78ee716fc5e69790136e8ee6ee1514c86486
baotq22 <87086420+baotq22@users.noreply.github.com>
2024-07-09 22:16:18 +0700

    Check compare card and add back button

--- src/components/memory-game/Card.jsx
+++ src/components/memory-game/Card.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
 
-export function Card({ cards, images, selectTwoCards }) {
+export function Card({ cards, images, selectTwoCards, disabled }) {
     const [isFold, setIsFold] = useState(true)
     const defaultBack = 'src/assets/img/mystery-box.png'
     const handleCardClick = () => {
         setIsFold(!isFold)
         selectTwoCards(cards)
     }
+    const handleCardClickNoFlip = () => {
+        setIsFold(!isFold)
+    }
     return (
         <div>
-            <div className="card-core" onClick={handleCardClick}>
+            <div className="card-core" onClick={isFold ? handleCardClick : handleCardClickNoFlip }>
                 <div className='card-flex'>
                     <div className={isFold ? "cardFold" : "cardUnfold"}>
                         <div className={isFold ? "" : "border"}>
@@ -18,6 +21,7 @@ export function Card({ cards, images, selectTwoCards }) {
                                 height={isFold ? "210px" : "180px"}
                                 width={isFold ? "150px" : "120px"}
                                 alt={isFold ? "Front" : "back"}
+                                disabled={disabled}
                             />
                         </div>
                     </div>
--- src/pages/MemoryGame.jsx
+++ src/pages/MemoryGame.jsx
@@ -1,43 +1,83 @@
 import { Card } from '../components/memory-game/Card'
 import '../components/scss/MMG.scss'
 import getLayout4 from '../assets/json/layout4.json'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import backButton from '../assets/img/thumb_back.png'
 
-export default function MemoryGame({card}) {
+export default function MemoryGame() {
+    const [play, setPlay] = useState(false)
     const [cards, setCards] = useState([])
     const [mark, setMark] = useState(0)
     const [firstSelect, setFirstSelect] = useState(null)
     const [secondSelect, setSecondSelect] = useState(null)
+
+    // back to main menu
+    const backToMain = () => {
+        setPlay(false)
+        setMark(0)
+        setFirstSelect(null)
+        setSecondSelect(null)
+    }
+
+    // shuffle card
     const shuffleCard = () => {
+        setPlay(true)
         const shuffleAciton = [...getLayout4, ...getLayout4]
             .sort(() => Math.random() - 0.5)
             .map((card) => ({ ...card, id: uuidv4() }))
 
         setCards(shuffleAciton)
-        setMark(0)
     }
 
     // console.log(cards)
 
+    // select 2 cards
     const selectTwoCards = (card) => {
         console.log(card)
+        firstSelect ? setSecondSelect(card) : setFirstSelect(card)
+    }
+
+    // compare card
+    useEffect(() => {
+        if (firstSelect && secondSelect) {
+            if (firstSelect.name === secondSelect.name) {
+                console.log("same")
+                ReFlip()
+            } else {
+                console.log("diff")
+                ReFlip()
+            }
+        }
+    }, [firstSelect, secondSelect])
+
+    // reset after match
+    const ReFlip = () => {
+        setFirstSelect(null)
+        setSecondSelect(null)
     }
     return (
         <>
             <div className="containerForPageGame">
                 <div className="containerForPageBody">
-                    <button onClick={shuffleCard}>New</button>
-                    <div className="card-container-4">
-                        {cards.map((card) => (
-                            <Card 
-                                key={card.id} 
-                                images={card.src}
-                                cards={card}
-                                selectTwoCards={selectTwoCards}
-                            />
-                        ))}
-                    </div>
+                    {!play && <button onClick={shuffleCard}>New</button>}
+                    {play &&
+                        <>
+                            <div className='topPage'>
+                                <img src={backButton} width="50px" height="50px" onClick={backToMain}/>
+                            </div>
+                            <div className="card-container-4">
+                                {cards.map((card) => (
+                                    <Card
+                                        key={card.id}
+                                        images={card.src}
+                                        cards={card}
+                                        selectTwoCards={selectTwoCards}
+                                        disabled={firstSelect === secondSelect}
+                                    />
+                                ))}
+                            </div>
+                        </>}
                 </div>
             </div>
         </>
